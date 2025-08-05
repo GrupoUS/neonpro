ec0fed326bffea3ba8c62e0228a110caaa30c951
GrupoUS <msm.jur@gmail.com>
2025-08-05 17:59:36 -0300

    cleanup: remove test files for pre-commit hooks

--- test-precommit-hooks.ts
+++
@@ -1,7 +0,0 @@
-// 🧪 Test file for pre-commit hooks
-const testFunction = () => {
-  console.log("Testing pre-commit hooks with TypeScript validation");
-  return "success";
-};
-
-export default testFunction;
--- test-precommit.ts
+++
@@ -1,6 +0,0 @@
-// Test file for pre-commit hooks validation
-const testFunction = (value: string): string => {
-  return value.toUpperCase();
-};
-
-export default testFunction;
